16b3a5b34c79108a8b9138f5ba177bd4e41f23c2
fraz14 <fraz140@gmail.com>
2020-01-20 13:11:58 +0100

    ...

--- overlays.mk
+++ overlays.mk
@@ -3,22 +3,7 @@ DEVICE_PACKAGE_OVERLAYS += vendor/overlay/common/
 
 # Extra packages
 PRODUCT_PACKAGES += \
-    AOSiPOverlayStub \
+    OverlayStub \
     PrimaryColorAOSiPBlackOverlay \
     PrimaryColorAOSiPNatureOverlay \
     PrimaryColorAOSiPOceanOverlay \
-
-# Accents
-include vendor/overlay/accents.mk
-
-# Bootanimation
-include vendor/overlay/bootanimation.mk
-
-# Fonts
-include vendor/overlay/fonts.mk
-
-# Sounds
-include vendor/overlay/sounds.mk
-
-# Prebuilts
-$(call inherit-product-if-exists, vendor/prebuilts/packages.mk)
